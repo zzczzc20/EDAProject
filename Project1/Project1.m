320cb3f95769793af978be776f09435d51b9e1ae
Zhou Zhongchun <zhoucc18@mails.tsinghua.edu.cn>
2020-11-30 14:55:42 +0800

    Add port state function.

--- Project1/Project1.m
+++ Project1/Project1.m
@@ -1,1 +1,8 @@
-
+function ret = Project1(TestFile)
+%Project1 - Description
+%
+% Syntax: ret = Project1(TestFile)
+%
+% Long description
+    
+end
--- Project1/port.m
+++ Project1/port.m
@@ -1,15 +1,23 @@
 classdef port
     properties
         cell = [];
+        state;
         id;
     end
     methods
         function obj = port(name)
             obj.cell = [];
             obj.id = id;
+            obj.state = 0;
         end
         function obj = AddCell(obj, MOS_id)
             obj.cell = [obj.cell MOS_id];
+        end
+        function obj = Discover(obj)
+            obj.state = 1;
+        end
+        function obj = Finish(obj)
+            obj.state = 2;
         end
     end
 end
