3989ae3d81415563d1ff134f61504aa632d07f1f
Zhou Zhongchun <zhoucc18@mails.tsinghua.edu.cn>
2020-12-02 15:32:00 +0800

    Initial design of Sets constructor.

--- Project1/Project1.m
+++ Project1/Project1.m
@@ -6,10 +6,28 @@ function ret = Project1(TestFile)
 % Long description
     % First Part ------------------------------------------------------------------------------
     % Construct ports set and MOS set ---------------------------------------------------------
+    FileId = fopen(TestFile);
+    Raw = textscan(FileId, '%c');
+    RawStr = Raw{1};
+    MOSNumber = double(RawStr(1) - '0');
+    NetNumber = double(RawStr(2) - '0');
 
     PortsSet = SET();
     MOSSet = SET();
 
+    for netid = 1:NetNumber
+        PortsSet = PortsSet.push(port(netid));
+    end
+    
+    for mosid = 1:MOSNumber
+        MOSDrain = double(RawStr(mosid * 3) - '0') + 1;
+        MOSSource = double(RawStr(mosid * 3 + 1) - '0') + 1;
+        MOSGate = RawStr(mosid * 3 + 2);
+        MOSSet = MOSSet.push(MOS(MOSDrain, MOSGate, MOSSource, mosid));
+        PortsSet.content(int32(MOSDrain)) = PortsSet.content(int32(MOSDrain)).AddCell(mosid);
+        PortsSet.content(int32(MOSSource)) = PortsSet.content(int32(MOSSource)).AddCell(mosid);
+    end
 
+    fclose(FileId);
     % The End of First Part -------------------------------------------------------------------
 end
