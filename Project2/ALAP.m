7561428a73990cce6cbc87bfede9c865eb463a5b
Zhou Zhongchun <zhoucc18@mails.tsinghua.edu.cn>
2020-12-13 23:17:35 +0800

    Try to solve the delay problem.

---
+++ Project2/ALAP.m
@@ -0,0 +1,8 @@
+function TargetMethod = ALAP(NodeSet, DependencySet, ASAPMethod)
+%ALAP - Description
+%
+% Syntax: TargetMethod = ALAP(NodeSet, DependencySet, ASAPMethod)
+%
+% Long description
+
+end
---
+++ Project2/ExtensionAfterASAP.m
@@ -0,0 +1,8 @@
+function [NodeSet, DependencySet, ASAPMethod] = ExtensionAfterASAP(NodeSet, DependencySet, ASAPMethod)
+%ExtensionAfterASAP - Description
+%
+% Syntax: [NodeSet, DependencySet, ASAPMethod] = ExtensionAfterASAP(NodeSet, DependencySet, ASAPMethod)
+%
+% Long description
+    
+end
