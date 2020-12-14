6fec3f529d4bc8b62e3dc764700a168754d76847
Zhou Zhongchun <zhoucc18@mails.tsinghua.edu.cn>
2020-12-14 15:04:04 +0800

    Initial design of the random solution generator.

--- Project2/RandomSolutionGenerator.m
+++ Project2/RandomSolutionGenerator.m
@@ -4,5 +4,15 @@ function Method = RandomSolutionGenerator(SrcMethod, NodeSet, DependencySet)
 % Syntax: Method = RandomSolutionGenerator(SrcMethod, NodeSet, DependencySet)
 %
 % Long description
-    
+    AvailableRangeX = AvailableRange(SrcMethod, NodeSet, DependencySet);
+    NumOfNode = NodeSet.size();
+    ChangeWho = 0;
+    while (true)
+        ChangeWho = randi([1, NumOfNode]);
+        if(AvailableRangeX.SingleVariationEarly(ChangeWho) ~=AvailableRangeX.SingleVariationLate(ChangeWho))
+            break;
+        end
+    end
+    SrcMethod.StartTime(ChangeWho) = randi([AvailableRangeX.SingleVariationEarly(ChangeWho), AvailableRangeX.SingleVariationLate(ChangeWho)]);
+    Method = SrcMethod;
 end
