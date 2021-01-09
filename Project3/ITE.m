bfd86d7f70189a9c044b73916f47e7a7a136f1bf
Zhou Zhongchun <zhoucc18@mails.tsinghua.edu.cn>
2021-01-09 11:49:54 +0800

    Finish the constructor of ITE.

--- Project3/ITE.m
+++ Project3/ITE.m
@@ -32,28 +32,229 @@ classdef ITE
     end
     methods
         function obj = ITE(InputString)
+            for index = 1:length(InputString)
+                if (InputString(index) == '=')
+                    CI = index;
+                    break;
+                end
+            end
+            obj.id = DeriveIdFromText(InputString(1:(CI - 1)));
+            for index = CI:length(InputString)
+                if (InputString(index) == 'x' || InputString(index) == 'F' || InputString(index) == '0' || InputString(index) == '1')
+                    CI = index;
+                    break;
+                end
+            end
+            FirstChar = InputString(CI);
+            FirstStart = CI;
+            for index = CI:length(InputString)
+                if (InputString(index) == ',')
+                    CI = index;
+                    obj.FirstType = FirstChar;
+                    break;
+                end
+                if (InputString(index) == '+')
+                    CI = index;
+                    obj.FirstType = '+';
+                    break;
+                end
+                if (InputString(index) == '-')
+                    CI = index;
+                    obj.FirstType = '-';
+                    break;
+                end
+            end
+            if (FirstChar == 'x')
+                obj.FirstVaribleId = DeriveIdFromText(InputString(FirstStart:(CI - 1)));
+            else
+                if (FirstChar == 'F')
+                    obj.FirstFunctionId = DeriveIdFromText(InputString(FirstStart:(CI - 1)));
+                end
+            end
+            if (obj.FirstType == '+')
+                for index = CI:length(InputString)
+                    if (InputString(index) == 'x')
+                        CI = index;
+                        FirstStart = index;
+                        break;
+                    end
+                end
+                for index = CI:length(InputString)
+                    if (InputString(index) == ',')
+                        CI = index;
+                        break;
+                    end
+                end
+                obj.FirstVaribleId = DeriveIdFromText(InputString(FirstStart:(CI - 1)));
+            else
+                if (obj.FirstType == '-')
+                    for index = CI:length(InputString)
+                        if (InputString(index) == 'x')
+                            CI = index;
+                            FirstStart = index;
+                            break;
+                        end
+                    end
+                    for index = CI:length(InputString)
+                        if (InputString(index) == ',')
+                            CI = index;
+                            break;
+                        end
+                    end
+                    obj.FirstVaribleId = DeriveIdFromText(InputString(FirstStart:(CI - 1)));   
+                end           
+            end
+
+
+
+            for index = CI:length(InputString)
+                if (InputString(index) == 'x' || InputString(index) == 'F' || InputString(index) == '0' || InputString(index) == '1')
+                    CI = index;
+                    break;
+                end
+            end
+            FirstChar = InputString(CI);
+            FirstStart = CI;
+            for index = CI:length(InputString)
+                if (InputString(index) == ',')
+                    CI = index;
+                    obj.SecondType = FirstChar;
+                    break;
+                end
+                if (InputString(index) == '+')
+                    CI = index;
+                    obj.SecondType = '+';
+                    break;
+                end
+                if (InputString(index) == '-')
+                    CI = index;
+                    obj.SecondType = '-';
+                    break;
+                end
+            end
+            if (FirstChar == 'x')
+                obj.SecondVaribleId = DeriveIdFromText(InputString(FirstStart:(CI - 1)));
+            else
+                if (FirstChar == 'F')
+                    obj.SecondFunctionId = DeriveIdFromText(InputString(FirstStart:(CI - 1)));
+                end
+            end
+            if (obj.SecondType == '+')
+                for index = CI:length(InputString)
+                    if (InputString(index) == 'x')
+                        CI = index;
+                        FirstStart = index;
+                        break;
+                    end
+                end
+                for index = CI:length(InputString)
+                    if (InputString(index) == ',')
+                        CI = index;
+                        break;
+                    end
+                end
+                obj.SecondVaribleId = DeriveIdFromText(InputString(FirstStart:(CI - 1)));
+            else
+                if (obj.SecondType == '-')
+                    for index = CI:length(InputString)
+                        if (InputString(index) == 'x')
+                            CI = index;
+                            FirstStart = index;
+                            break;
+                        end
+                    end
+                    for index = CI:length(InputString)
+                        if (InputString(index) == ',')
+                            CI = index;
+                            break;
+                        end
+                    end
+                    obj.SecondVaribleId = DeriveIdFromText(InputString(FirstStart:(CI - 1)));   
+                end           
+            end
+
+
+
+
+
+
+
+
+
+
             
+            for index = CI:length(InputString)
+                if (InputString(index) == 'x' || InputString(index) == 'F' || InputString(index) == '0' || InputString(index) == '1')
+                    CI = index;
+                    break;
+                end
+            end
+            ThirdChar = InputString(CI);
+            ThirdStart = CI;
+            for index = CI:length(InputString)
+                if (InputString(index) == ')')
+                    CI = index;
+                    obj.ThirdType = ThirdChar;
+                    break;
+                end
+                if (InputString(index) == '+')
+                    CI = index;
+                    obj.ThirdType = '+';
+                    break;
+                end
+                if (InputString(index) == '-')
+                    CI = index;
+                    obj.ThirdType = '-';
+                    break;
+                end
+            end
+            if (ThirdChar == 'x')
+                obj.ThirdVaribleId = DeriveIdFromText(InputString(ThirdStart:(CI - 1)));
+            else
+                if (ThirdChar == 'F')
+                    obj.ThirdFunctionId = DeriveIdFromText(InputString(ThirdStart:(CI - 1)));
+                end
+            end
+            if (obj.ThirdType == '+')
+                for index = CI:length(InputString)
+                    if (InputString(index) == 'x')
+                        CI = index;
+                        ThirdStart = index;
+                        break;
+                    end
+                end
+                for index = CI:length(InputString)
+                    if (InputString(index) == ')')
+                        CI = index;
+                        break;
+                    end
+                end
+                obj.ThirdVaribleId = DeriveIdFromText(InputString(ThirdStart:(CI - 1)));
+            else
+                if (obj.ThirdType == '-')
+                    for index = CI:length(InputString)
+                        if (InputString(index) == 'x')
+                            CI = index;
+                            ThirdStart = index;
+                            break;
+                        end
+                    end
+                    for index = CI:length(InputString)
+                        if (InputString(index) == ')')
+                            CI = index;
+                            break;
+                        end
+                    end
+                    obj.ThirdVaribleId = DeriveIdFromText(InputString(ThirdStart:(CI - 1)));   
+                end           
+            end
+
         end
         function Print(obj)
             disp('id: '+ obj.id);
             disp('First: '+obj.FirstVaribleId+" Func: "+obj.FirstFunctionId+" Type: "+obj.FirstType);
             disp('Second: '+obj.SecondVaribleId+" Func: "+obj.SecondFunctionId+" Type: "+obj.SecondType);
             disp('Third: '+obj.ThirdVaribleId+" Func: "+obj.ThirdFunctionId+" Type: "+obj.ThirdType);
-        end
-    end
-    methods (Static)
-        function [Type, id, End, EndSymbol] = Scanner(InputString, begin)
-            % This basic type just have 2 possibilities: x or F
-            Type = InputString(begin);
-
-            for index = begin:length(InputString)
-                if (InputString(index) == ',' || InputString(index) == '+' || InputString(index) == ')' || InputString(index) == '-')
-                    break;
-                end
-            end
-            End = index;
-            id = DeriveIdFromText(InputString(begin:(End - 1)));
-            EndSymbol = InputString(End);
         end
     end
 end
