e7c3d855228e3a51b64046accb52f0832c218921
zzczzc20 <zhouzc2000@foxmail.com>
2021-01-12 21:14:37 +0800

    Test the set constructor.

--- Project3/Project3.m
+++ Project3/Project3.m
@@ -4,5 +4,12 @@ function Project3(TestFile)
 % Syntax: Projec3(TestFile)
 %
 % Long description
-    
+    % Test of set constructor
+    [SetOfIte, pmat, PrintList] = SetConstructor(TestFile);
+    %SetOfIte.Print();
+    for index = 1:SetOfIte.size()
+        SetOfIte.content(index)
+    end
+    NumOfVars = length(pmat);
+    PrintList
 end
--- Project3/SetConstructor.m
+++ Project3/SetConstructor.m
@@ -1,7 +1,7 @@
-function [SetOfITE, pmat] = SetConstructor(TestFile)
+function [SetOfIte, pmat, PrintList] = SetConstructor(TestFile)
 %SetConstructor - Description
 %
-% Syntax: [SetOfITE, pmat] = SetConstructor(TestFile)
+% Syntax: [SetOfIte, pmat, PrintList] = SetConstructor(TestFile)
 %
 % Long description
 FileId = fopen(TestFile);
@@ -10,7 +10,18 @@ NumOfIte = str2num(fgetl(FileId));
 pmat = zeros(1, NumOfVarible);
 TargetStr = fgetl(FileId);
 SetOfIte = SET([]);
+PrintList = [];
 for index = 1:NumOfIte
-    SetOfIte = SetOfIte.push(ITE(char(fgetl(FileId))));
+    CurrentStr = char(fgetl(FileId));
+    SetOfIte = SetOfIte.push(ITE(CurrentStr));
+end
+while ~feof(FileId)
+    tline = char(fgetl(FileId));
+    for index = 1:length(tline)
+        if (tline(index) == 'F')
+            break;
+        end
+    end
+    PrintList = [PrintList DeriveIdFromText(tline(index:length(tline)))];
 end
 end
